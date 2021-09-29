005240e922bf3ce8d93d3f8d46bb725dd0b06c58
Jessie_Zheng <39744779+Zheng612@users.noreply.github.com>
2021-09-29 12:18:08 -0500

    Update lc_1470_shuffleArray.py

--- python/leetcode/lc_1470_shuffleArray.py
+++ python/leetcode/lc_1470_shuffleArray.py
@@ -1,3 +1,6 @@
+# Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+# Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
 class Solution(object):
     def shuffle(self, nums, n):
         nums1=nums[0:n]
