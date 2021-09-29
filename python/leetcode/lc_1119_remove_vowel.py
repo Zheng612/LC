3f6ba083c548ea9a3fce8ecc1080aa45027ebf87
Jessie_Zheng <39744779+Zheng612@users.noreply.github.com>
2021-09-29 12:11:31 -0500

    Update lc_1119_remove_vowel.py

--- python/leetcode/lc_1119_remove_vowel.py
+++ python/leetcode/lc_1119_remove_vowel.py
@@ -1,3 +1,6 @@
+# Given a string s, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
+
+
 class Solution(object):
     def removeVowels(self, s):
         return "".join([i for i in s if i not in ['a','e','i','o','u']])
