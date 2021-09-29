128f8a5a73fa028702e88df5365e9368185985c0
Jessie_Zheng <39744779+Zheng612@users.noreply.github.com>
2021-09-29 12:19:23 -0500

    Update lc_1512_GoodPairs.py

--- python/leetcode/lc_1512_GoodPairs.py
+++ python/leetcode/lc_1512_GoodPairs.py
@@ -1,3 +1,6 @@
+# Given an array of integers nums, return the number of good pairs.
+# A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
 class Solution(object):
     def numIdenticalPairs(self, nums):
         i=0
