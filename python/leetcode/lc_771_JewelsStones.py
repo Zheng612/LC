6a95d135954ccb7a5a6d64e64554733997270cfd
Jessie_Zheng <39744779+Zheng612@users.noreply.github.com>
2021-09-29 12:22:10 -0500

    Update lc_771_JewelsStones.py

--- python/leetcode/lc_771_JewelsStones.py
+++ python/leetcode/lc_771_JewelsStones.py
@@ -1,3 +1,8 @@
+# You're given strings jewels representing the types of stones that are jewels, 
+# and stones representing the stones you have. Each character in stones is a type of stone you have. 
+# You want to know how many of the stones you have are also jewels.
+# Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
         i=0
